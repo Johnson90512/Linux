10ff89b21dd13a9eb24e85bbdebe4f43e6571c70
kyle <johnson90512@gmail.com>
2019-02-16 18:21:53 -0500

    Created an array

--- LinuxBuilder.py
+++ LinuxBuilder.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python
 import os
 
+#kaliTools = ["dmitry", "dnmap", "dnsenum", "dnsmap", "dnsrecon", "dnstracer", "dnswalk", "dotdotpwn", "enum4linux", "exploitdb", "fierce", "firewalk", "masscan", "nmap", "recon-ng", "smtp-user-enum", "urlcrazy", "wol-e", "hping3", "cisco-auditing-tool", "cisco-global-exploiter", "cisco-ocs", "cisco-torch", "copy-router-config", "hexorbase", "aircrack-ng", "asleap", "bluelog", "blueranger", "bluesnarfer", "pixiewps", "reaver", "bbqsql", "blindelephant", "burpsuite", "dirb", "dirbuster", "wfuzz", "wpscan", "zaproxy", "responder", "wireshark", "cryptcat", "cymothoa", "dbd", "dns2tcp",  "httptunnel", "casefile", "cutycapt", "dos2unix", "dradis", "keepnote",  "metagoofil", "nipper-ng", "pipal", "armitage", "jboss-autopwn", "linux-exploit-suggester", "shellnoob", "beef-xss", "binwalk", "p0f", "pdf-parser", "dhcpig", "cewl", "john", "johnny", "wordlists", "radare2"]
+
 os.system("apt-get install -y dmitry dnmap dnsenum dnsmap dnsrecon dnstracer dnswalk dotdotpwn enum4linux exploitdb fierce firewalk masscan nmap recon-ng smtp-user-enum urlcrazy wol-e hping3 cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch copy-router-config hexorbase aircrack-ng asleap bluelog blueranger bluesnarfer pixiewps reaver bbqsql blindelephant burpsuite dirb dirbuster wfuzz wpscan zaproxy responder wireshark cryptcat cymothoa dbd dns2tcp  httptunnel casefile cutycapt dos2unix dradis keepnote  metagoofil nipper-ng pipal armitage jboss-autopwn linux-exploit-suggester shellnoob beef-xss binwalk p0f pdf-parser dhcpig cewl john johnny wordlists radare2")
-#os.system("apt-get install -y cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch copy-router-config hexorbase")
-#os.system("apt-get install -y aircrack-ng asleap bluelog blueranger bluesnarfer pixiewps reaver")
-#os.system("apt-get install -y bbqsql blindelephant burpsuite dirb dirbuster wfuzz wpscan zaproxy")
-#os.system("apt-get install -y responder wireshark")
-#os.system("apt-get install -y cryptcat cymothoa dbd dns2tcp  httptunnel")
-#os.system("apt-get install -y casefile cutycapt dos2unix dradis keepnote  metagoofil nipper-ng pipal")
-#os.system("apt-get install -y armitage jboss-autopwn linux-exploit-suggester shellnoob beef-xss")
-#os.system("apt-get install -y binwalk p0f pdf-parser")
-#os.system("apt-get install -y dhcpig")
-#os.system("apt-get install -y cewl john johnny wordlists")
-#os.system("apt-get install -y radare2")
 os.system("mkdir ~/scripts")
 os.system("git clone https://github.com/rebootuser/LinEnum ~/scripts/linenum")
 os.system("git clone https://github.com/DominicBreuker/pspy ~/scripts/pspy")
